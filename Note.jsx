a4a71ded0aa9f63060a2164139edb43ff334e6ab
AKG79 <85401871+AKG79@users.noreply.github.com>
2021-06-07 06:44:50 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Note() {
+  return (
+          <div>
+              <div className="note">
+                    <h1>Javascript and React.js</h1>
+                    <p>
+                          It was an amazing bootcamp taken from Shaurya Sinha, I learnt a lot of skills from this bootcamp. We taught some fundamental programming of JS react with html .
+                    </p>
+              </div>
+              <div className="note">
+                    <h1>Python Machine Leaning</h1>
+                    <p>
+                          I had joined the Python with machiny learning bootcay, this bootcamp helped me data analytics as well as I covered the various topics in this bootcamp numpy, pandas, scikit etc.
+                    </p>
+              </div>
+          </div>
+  );
+}
+
+export default Note;
